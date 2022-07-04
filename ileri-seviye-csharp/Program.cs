8803d0fba48917a140d373bf94178a198ddce343
Ümit Atalan <umit.atalan@gmail.com>
2022-07-04 15:44:26 +0300

    var, object ve dynamic işleçleri

--- ileri-seviye-csharp/Program.cs
+++ ileri-seviye-csharp/Program.cs
@@ -6,7 +6,23 @@ namespace ileri_seviye_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // var işleci verinin tipinin belli olmadığı durumlarda kullanılır.
+
+            //object bütün sınıfların temel sınıfıdır.
+
+            object x = 5;
+            object y = "merhaba";
+
+            string mesaj = (string)y;
+            Console.WriteLine(mesaj);
+
+            //dynamic geliştirme aşamasında hata göstermez ancak çalışma anında hata verir. Dinamik programlamaya imkan verir.a
+
+            dynamic kelime = "merhaba";
+            Console.WriteLine(kelime.ToUpper());
+
+
+            Console.ReadLine();
         }
     }
 }
