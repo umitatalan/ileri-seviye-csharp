06a28e3253bccff169a30151c12ccd969fadf613
Ümit Atalan <umit.atalan@gmail.com>
2022-07-04 16:20:09 +0300

    params anahtar sözcüğü

--- ileri-seviye-csharp/Program.cs
+++ ileri-seviye-csharp/Program.cs
@@ -7,31 +7,21 @@ namespace ileri_seviye_csharp
     {
         static void Main(string[] args)
         {
-            int[] sayilar = new int[5] { 1,2,3,4,5 };
-
-            List<int> numbers = new List<int>(sayilar);
-            numbers.Add(21);
-            numbers.Add(1);
-
-            foreach (var item in numbers)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("-----");
-
-            numbers.Remove(4);
-
-            foreach (var item in numbers)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("Dizi uzunluğu : " + numbers.Count);
-            Console.WriteLine(numbers.BinarySearch(21));
-            Console.WriteLine(numbers[5]);
-
+            // params anahtar sözcüğü
+            Console.WriteLine(topla(1,5,6, 8,9,11,-6,44, 51));
             Console.ReadLine();
+        }
+
+        static int topla(params int[] sayilar)
+        {
+            int toplam = 0;
+
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+            }
+
+            return toplam;
         }
     }
 }
