491238e24fbc8c50a905a013cab127c95b57c7d1
Ümit Atalan <umit.atalan@gmail.com>
2022-07-05 09:10:47 +0300

    method overloading

--- ileri-seviye-csharp/Program.cs
+++ ileri-seviye-csharp/Program.cs
@@ -8,20 +8,43 @@ namespace ileri_seviye_csharp
 
         static void Main(string[] args)
         {
-            Kisi ilkkisi = new Kisi("ABC", "XYZ", 37);
-            ilkkisi.OzellikleriYaz();
+            //Overloading
+
+            Console.WriteLine(topla(5,1));
+            Console.WriteLine(topla(1,7,3));
+            Console.WriteLine(topla(5.3,5.9));
+
+            yazdir(5);
+            yazdir("Text");
 
             Console.ReadLine();
         }
-    }
 
-    class Sinif
-    {
-        public int x = 1;
-
-        public void yazdir(string text)
+        static int topla(int a, int b)
         {
-            Console.WriteLine(text);
+            return a + b;
         }
+
+        static int topla(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        
+        static double topla(double a, double b)
+        {
+            return a + b;
+        }
+        
+        static void yazdir(int sayi)
+        {
+            Console.WriteLine(sayi.ToString());
+        }
+
+        static void yazdir(string metin)
+        {
+            Console.WriteLine(metin);
+        }
+
+
     }
 }
