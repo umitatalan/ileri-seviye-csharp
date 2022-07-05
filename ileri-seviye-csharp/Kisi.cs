6647883a32097ccee59e8437f24fb676d9610979
Ümit Atalan <umit.atalan@gmail.com>
2022-07-05 08:51:32 +0300

    Sınıflar

---
+++ ileri-seviye-csharp/Kisi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ileri_seviye_csharp
+{
+    class Kisi
+    {
+        public string Ad;
+        public string Soyad;
+        public int Yas;
+
+        public void OzellikleriYaz()
+        {
+            Console.WriteLine("{0}\n{1}\n{2}", Ad, Soyad, Yas);
+        }
+    }
+}
--- ileri-seviye-csharp/Program.cs
+++ ileri-seviye-csharp/Program.cs
@@ -5,12 +5,33 @@ namespace ileri_seviye_csharp
 {
     class Program
     {
-        const double PI = 3.14;
+
         static void Main(string[] args)
         {
-            // const anahtar sözcüğü değerin değiştirilemeyeceği verilerde kullanılır.
-           
+            Sinif sinif = new Sinif();
+            Console.WriteLine(sinif.x);
+            sinif.yazdir("Merhaba");
+
+            Kisi kisi1 = new Kisi();
+            kisi1.Ad = "Ahmet";
+            kisi1.Soyad = "Mehmet";
+            kisi1.Yas = 39;
+            kisi1.OzellikleriYaz();
+
+            Kisi kisi2 = new Kisi() { Ad = "Mehmet", Soyad = "Alnıaçık", Yas = 41 };
+            kisi2.OzellikleriYaz();
+
             Console.ReadLine();
+        }
+    }
+
+    class Sinif
+    {
+        public int x = 1;
+
+        public void yazdir(string text)
+        {
+            Console.WriteLine(text);
         }
     }
 }
